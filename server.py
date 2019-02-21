1889730d19ccbdda56e7a11c0d7b0fe8616a7749
hermanthegerman2 <doetsch.hermann@t-online.de>
2019-02-22 00:10:56 +0100

    initial commit

--- server.py
+++ server.py
@@ -5,23 +5,19 @@ import connexion
 
 
 # Read the swagger.yml file to configure the endpoints
-
-
-# Create a URL route in our application for "/"
-
-
 # If we're running in stand alone mode, run the application
 if __name__ == '__main__':
     app = connexion.App(__name__, 5000, specification_dir='./')
     app.add_api('swagger.yml', resolver=RestyResolver('api'))
     app.run(host='0.0.0.0', port=5000, debug=True)
 
-
+# Create a URL route in our application for "/"
 @app.route('/')
-    def home():
-        """
-        This function just responds to the browser ULR
-        localhost:5000/
-        :return:        the rendered template 'home.html'
-        """
-        return render_template('home.html')
+def home():
+    """
+    This function just responds to the browser ULR
+    localhost:5000/
+    :return:        the rendered template 'home.html'
+    """
+    return render_template('home.html')
+
