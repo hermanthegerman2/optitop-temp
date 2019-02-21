7be9c1f3f588217fff8112c2cebd79ab13df904e
hermanthegerman2 <doetsch.hermann@t-online.de>
2019-02-22 00:22:34 +0100

    initial commit

--- server.py
+++ server.py
@@ -9,16 +9,17 @@ from connexion.resolver import RestyResolver
 # If we're running in stand alone mode, run the application
 
 # Create a URL route in our application for "/"
-@app.route('/')
-def home():
-    """
-    This function just responds to the browser ULR
-    localhost:5000/
-    :return:        the rendered template 'home.html'
-    """
-    return render_template('home.html')
+
+
 
 if __name__ == '__main__':
     app = connexion.App(__name__, 5000, specification_dir='./')
+    def home():
+        """
+        This function just responds to the browser ULR
+        localhost:5000/
+        :return:        the rendered template 'home.html'
+        """
+        return render_template('home.html')
     app.add_api('swagger.yml', resolver=RestyResolver('api'))
     app.run(host='0.0.0.0', debug=True)
