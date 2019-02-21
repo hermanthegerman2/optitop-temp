d5843323808163859717a0a52b550773f2832366
hermanthegerman2 <doetsch.hermann@t-online.de>
2019-02-22 00:25:13 +0100

    initial commit

--- server.py
+++ server.py
@@ -1,25 +1,22 @@
-import connexion
 from flask import render_template
-from connexion.resolver import RestyResolver
+import connexion
 
 # Create the application instance
-
+app = connexion.App(__name__, specification_dir='./')
 
 # Read the swagger.yml file to configure the endpoints
-# If we're running in stand alone mode, run the application
+app.add_api('swagger.yml')
 
 # Create a URL route in our application for "/"
+@app.route('/')
+def home():
+    """
+    This function just responds to the browser ULR
+    localhost:5000/
+    :return:        the rendered template 'home.html'
+    """
+    return render_template('home.html')
 
-
-
+# If we're running in stand alone mode, run the application
 if __name__ == '__main__':
-    app = connexion.App(__name__, 5000, specification_dir='./')
-    def home():
-        """
-        This function just responds to the browser ULR
-        localhost:5000/
-        :return:        the rendered template 'home.html'
-        """
-        return render_template('home.html')
-    app.add_api('swagger.yml', resolver=RestyResolver('api'))
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
